40780d520c120ef4fe92b432cf7cc60d3fe2c533
Utsav Somaiya <utsav.infynno@gmail.com>
2025-06-22 20:27:44 +0530

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,8 +9,9 @@
             x-on:update-title.window="title = $event.detail.title"
         ></title>
 
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@400;500;700&display=swap" rel="stylesheet">
 
         @livewireStyles
         @vite('resources/js/app.js')
