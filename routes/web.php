1637f06fb8fc392ef5d1a1e2224e92183c73da4a
Utsav Somaiya <utsav.infynno@gmail.com>
2025-06-22 19:59:23 +0530

    wip

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Livewire\Demo;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
-Log::info('on `master` branch');
+Log::channel('daily')->info('on `master` branch');
 
 Route::get('/', Demo::class)->name('demo');
